2714759a121b795c93ae17a9af89de0561295311
heenahan <mby1997s@gmail.com>
2023-07-23 00:50:20 +0900

    fix : personnel에 대한 검증 로직 수행 안됨

--- src/main/java/com/prgms/springbootrestapi/domain/Personnel.java
+++ src/main/java/com/prgms/springbootrestapi/domain/Personnel.java
@@ -6,16 +6,16 @@ public record Personnel(
     int best
 ) {
     public Personnel {
-        validMin();
-        validBest();
+        validMin(max, min);
+        validBest(max, min, best);
     }
-    private void validMin() {
+    private void validMin(int max, int min) {
         if (min > max) {  // 최대 인원수보다 크다면
             throw new IllegalArgumentException("최소 인원은 최대 인원보다 클 수 없습니다.");
         }
     }
 
-    private void validBest() {
+    private void validBest(int max, int min, int best) {
         if (best < min || best > max) {
             throw new IllegalArgumentException("베스트 인원은 최소 인원보다 크고 최대 인원보다 작아야 합니다.");
         }
